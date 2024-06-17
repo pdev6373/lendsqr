a727cbb0a46420f0051f3c2d5e55e339eca569da
Peter <pdev6373@gmail.com>
2024-06-17 14:23:17 +0100

    update: work on header and sidenav responsiveness

--- components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Input } from './input/Input';
-export { default as Header } from './header/Header';
-export { default as Sidenav } from './sidenav/Sidenav';
-export { default as Search } from './search/Search';
